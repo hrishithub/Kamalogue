25ba8afd6c65ed98fbccb36f5eb30304edcee8fd
Hrishikesh <hrishikeshgawde2002@gmail.com>
2023-04-09 20:29:36 +0530

    commented import css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React , {useEffect , useState} from 'react';
 import { Layout } from '../components';
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
 import '../styles/globals.scss'
 
 
