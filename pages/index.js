794f0675ff29b975e68cc17464d24575acc8e54c
Hrishikesh <hrishikeshgawde2002@gmail.com>
2023-04-12 20:02:38 +0530

    here we go

--- pages/index.js
+++ pages/index.js
@@ -11,31 +11,47 @@ const posts = [
 ]
 
 export default function Home({posts}) {
-  
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="container mx-auto px-10 mb-8 bg-gray"> 
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 lg:p-24">
+      <div className="container mx-auto px-4 lg:px-10 mb-8 bg-gray-200 lg:bg-transparent"> 
         <title>Kamalogue</title> 
 
         <FeaturedPosts />
         
-       
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'> 
-        
           <div className='lg:col-span-8 col-span-1'>
             {posts.map((post , index) =>  <PostCard post={post.node} key={post.title}/>)}
           </div>
           <div className='lg:col-span-4 col-span-1'>
             <div className='lg:sticky relative top-8'>
-            <PostWidget /> 
-      
-            <Categories />
-
+              <PostWidget />
+              <Categories />
             </div>
           </div>
         </div>
       </div>
+      <style jsx>{`
+        .container {
+          padding: 0 16px;
+        }
+
+        h1 {
+          font-size: 2rem;
+        }
+
+        @media (min-width: 640px) {
+          h1 {
+            font-size: 3rem;
+          }
+        }
+
+        @media (min-width: 768px) {
+          .container {
+            padding: 0;
+          }
+        }
+      `}</style>
+
     </main>
   )
 } 
