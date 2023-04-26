1977d67500ba63db94269fc7202b93a63f6168e6
Hrishikesh <hrishikeshgawde2002@gmail.com>
2023-04-26 23:40:08 +0530

    font changed

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Add your favicon link here */}
+        <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
