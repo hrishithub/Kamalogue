fcc0aa7f6e2df5d015b5b9d62909796658a65852
Hrishikesh <hrishikeshgawde2002@gmail.com>
2023-04-26 23:34:26 +0530

    msg

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import '../styles/globals.scss'
  function MyApp({ Component, pageProps }) {
   return( 
   <Layout>
-    <Component {...pageProps} />
+    <Component {...pageProps}/>
   </Layout>
   ) 
 } 
